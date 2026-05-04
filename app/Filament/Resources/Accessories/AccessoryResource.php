5b6623550a34d48a9130e18a8b045c8cc1759f30
danangprass <test.danangprass@gmail.com>
2026-05-04 22:15:48 +0700

    fix(admin): restore Accessories nav and hide AccessoryImages instead
    Accessory images should be managed via the relation manager on the
    Accessory resource, not as a standalone top-level menu item.
    
    Refs: #2

--- app/Filament/Resources/Accessories/AccessoryResource.php
+++ app/Filament/Resources/Accessories/AccessoryResource.php
@@ -20,8 +20,6 @@ class AccessoryResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
-    protected static bool $shouldRegisterNavigation = false;
-
     public static function form(Schema $schema): Schema
     {
         return AccessoryForm::configure($schema);
