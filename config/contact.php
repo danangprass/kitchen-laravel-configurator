d77509a37e9965af2e2f04228ba8b60ac14f57e5
danangprass <test.danangprass@gmail.com>
2026-05-09 11:16:24 +0700

    refactor: complete footer audit — configurable contact info, social links, improved newsletter UX
    Closes #46

---
+++ config/contact.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'email' => env('CONTACT_EMAIL', 'info@kitchen.com'),
+    'phone' => env('CONTACT_PHONE', '+62 819 0885 2999'),
+    'address' => env('CONTACT_ADDRESS', 'Jakarta, Indonesia'),
+
+    'social' => [
+        'facebook' => env('SOCIAL_FACEBOOK', 'https://facebook.com/kitchenindonesia'),
+        'instagram' => env('SOCIAL_INSTAGRAM', 'https://instagram.com/kitchenindonesia'),
+        'youtube' => env('SOCIAL_YOUTUBE', 'https://youtube.com/@kitchenindonesia'),
+    ],
+];
